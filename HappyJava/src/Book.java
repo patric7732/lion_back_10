a873949684a7885bfc3d2caa2373ced01e7d8d00
carami <carami@nate.com>
2024-03-12 14:25:48 +0900

    update exam

--- HappyJava/src/Book.java
+++ HappyJava/src/Book.java
@@ -1,6 +1,6 @@
 public class Book {
     private String title;
-    private int price;
+     static int price;
 
     //메소드
     //접근제한자 리턴타입 메소드명(매개변수들... ){  구현  }
---
+++ HappyJava/src/OpExam2.java
@@ -0,0 +1,21 @@
+public class OpExam2 {
+    public static void main(String[] args) {
+        int a = 5;
+        ++a; // a = a + 1   a+= 1
+
+        System.out.println(++a);
+
+        System.out.println(a);
+        int b = 10;
+        System.out.println(b+4);
+
+        System.out.println(b);
+
+//        Book book = new Book();
+//        book.getPrice();
+
+//        Book.price = 10;
+
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
