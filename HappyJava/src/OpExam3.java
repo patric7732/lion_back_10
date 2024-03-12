f967b5d40e5ec5defe303fea3f311f7f3944dc9b
carami <carami@nate.com>
2024-03-12 16:13:15 +0900

    update exam

---
+++ HappyJava/src/OpExam3.java
@@ -0,0 +1,14 @@
+public class OpExam3 {
+    public static void main(String[] args) {
+        // 조건식 ? 반환값1 : 반환값2
+        int a = 10;
+        int b = 20;
+        int result = a > b ? a : b;
+
+        System.out.println(result);
+        Book book = new Book();
+        if(book instanceof Object){
+            System.out.println("Book 은 객체 입니다.");
+        }
+    }
+}
