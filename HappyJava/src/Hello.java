0ca2eadcf2503fff32dca3d30da974f36ae178ad
carami <carami@nate.com>
2024-03-12 13:40:49 +0900

    update exam

--- HappyJava/src/Hello.java
+++ HappyJava/src/Hello.java
@@ -1,10 +1,15 @@
 import java.io.PrintStream;
 
 public class Hello {
+
+
+    String name;
+    int age;
+
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello Java");
 
-//        PrintStream out ;
+        /** PrintStream out ; */
     }
 }
---
+++ HappyJava/src/IntPrint.java
@@ -0,0 +1,28 @@
+public class IntPrint {
+    public static void main(String[] args) {
+        byte b;
+        short  s;
+//        int i = 0;
+        long l ;
+        float f ;
+        double d ;
+        char c;
+        boolean flag;
+
+        int i = 2;
+        int ii = 1+1;
+
+        c = 10;
+//        flag = 'a';
+        System.out.println(c);
+
+        System.out.println();
+
+        String str = "hello";
+        str = "hi";
+
+        Integer iii = 10;
+
+
+    }
+}
