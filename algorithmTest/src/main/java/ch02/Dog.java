42eed01c45370894187782c1af2ef995d46bdf0f
carami <carami@nate.com>
2024-03-20 15:55:17 +0900

    update exam

--- algorithmTest/src/main/java/ch02/Dog.java
+++ algorithmTest/src/main/java/ch02/Dog.java
@@ -36,4 +36,20 @@ public class Dog {
         System.out.println("Breed: " + breed + " Age: " + age + " Color: " + color);
     }
 
+    static void classMethod(){
+        System.out.println("클래스 메서드는 인스턴스화 하지 않아도 사용할 수 있어요. ");
+    }
+
+    void instanceMethod(){
+        System.out.println("인스턴스 메소드는 반드시 인스턴스화 한 후 사용 할 수 있어요.");
+    }
+    public static void main(String[] args) {
+        Dog.classMethod();  //클래스로 바로 접근해서 사용이 가능..
+
+        Dog dog = new Dog();
+        dog.instanceMethod(); //인스턴스를 통해서만 접근가능!!
+
+
+    }
+
 }
