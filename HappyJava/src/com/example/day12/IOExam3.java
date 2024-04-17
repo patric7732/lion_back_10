3a3ed51404490c5d2e82dafc2db5dd49f3391148
carami <carami@nate.com>
2024-04-17 13:59:23 +0900

    update exam

---
+++ HappyJava/src/com/example/day12/IOExam3.java
@@ -0,0 +1,10 @@
+package com.example.day12;
+
+public class IOExam3 {
+    public static void main(String[] args) {
+        //"test.txt"파일에서 한 줄 씩 입력받아서 입력받은 문자뒤에 hi 를 추가해서
+        // "testhi.txt" 파일에 출력하는 프로그램을 작성해보세요.
+
+        
+    }
+}
