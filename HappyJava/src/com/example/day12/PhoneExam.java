d7eb2cf17fbe380a7781afde909796f42b0c2036
carami <carami@nate.com>
2024-04-18 11:00:19 +0900

    add Thread exam

--- HappyJava/src/com/example/day12/PhoneExam.java
+++ HappyJava/src/com/example/day12/PhoneExam.java
@@ -8,9 +8,9 @@ public class PhoneExam {
         String phoneNumber = null;
         String phoneList = null;
 
-        try (PrintWriter outTxt = new PrintWriter(new FileOutputStream("c:\\temp\\phone.txt"));
+        try (PrintWriter outTxt = new PrintWriter(new FileOutputStream("c:\\temp\\phone.txt", true));
              BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));) {
-            
+
             for (int count = 1; count <= 3; count++) {
                 System.out.print("이름: ");
                 name = kb.readLine();
@@ -20,19 +20,22 @@ public class PhoneExam {
             }
             outTxt.close();
             System.out.println("... 전화번호가 c:\\temp\\phone.txt에 저장되었습니다.");
-
-            BufferedReader br = new BufferedReader(new FileReader("c:\\temp\\phone.txt"));
-            System.out.println();
-            System.out.println("c:\\temp\\phone.txt의 내용은 다음과 같습니다...");
-            while ((phoneList = br.readLine()) != null) {
-                System.out.println(phoneList);
-            }
         } catch (FileNotFoundException e) {
             System.out.println("Error opening the file." + e.getMessage());
             System.exit(0);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader("c:\\temp\\phone.txt"));) {
+            System.out.println();
+            System.out.println("c:\\temp\\phone.txt의 내용은 다음과 같습니다...");
+            while ((phoneList = br.readLine()) != null) {
+                System.out.println(phoneList);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
