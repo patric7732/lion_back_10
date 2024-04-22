5f2691f823a0ddaa3f407c4608a4fd51e72689b8
carami <carami@nate.com>
2024-04-22 11:37:15 +0900

    add network exam

--- HappyJava/src/com/example/day13/ThreadJoinExample.java
+++ HappyJava/src/com/example/day13/ThreadJoinExample.java
@@ -1,5 +1,24 @@
 package com.example.day13;
 
+
+class TaskThread extends Thread {
+    private String taskName;
+
+    public TaskThread(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void run() {
+        System.out.println(taskName + " 작업 시작");
+        try {
+            Thread.sleep(2000);  // 2초 동안 스레드 일시 정지 (작업 시뮬레이션)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(taskName + " 작업 완료");
+    }
+}
+
 class DemonThread extends Thread {
     @Override
     public void run() {
@@ -15,23 +34,6 @@ class DemonThread extends Thread {
 }
 
 public class ThreadJoinExample {
-    static class TaskThread extends Thread {
-        private String taskName;
-
-        public TaskThread(String taskName) {
-            this.taskName = taskName;
-        }
-
-        public void run() {
-            System.out.println(taskName + " 작업 시작");
-            try {
-                Thread.sleep(2000);  // 2초 동안 스레드 일시 정지 (작업 시뮬레이션)
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(taskName + " 작업 완료");
-        }
-    }
 
     public static void main(String[] args) {
         TaskThread task1 = new TaskThread("작업 1");
