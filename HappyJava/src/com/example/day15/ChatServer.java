19d18519040744315e2dac344f11e148eecd0c0d
carami <carami@nate.com>
2024-04-23 11:53:00 +0900

    update exam

--- HappyJava/src/com/example/day15/ChatServer.java
+++ HappyJava/src/com/example/day15/ChatServer.java
@@ -16,6 +16,7 @@ public class ChatServer {
             System.out.println("서버가 준비되었습니다.");
             //여러명의 클라이언트의 정보를 기억할 공간
             Map<String, PrintWriter> chatClients = new HashMap<>();
+
             while (true) {
                 //2. accept() 를 통해서 소켓을 얻어옴.   (여러명의 클라이언트와 접속할 수 있도록 구현)
                 Socket socket = serverSocket.accept();
@@ -26,9 +27,8 @@ public class ChatServer {
             }
         } catch (Exception e) {
             System.out.println(e);
+            e.printStackTrace();
         }
-
-
     }
 }
 
@@ -38,36 +38,39 @@ class ChatThread extends Thread {
     private String id;
     private Map<String, PrintWriter> chatClients;
 
-    BufferedReader in = null;
+    private BufferedReader in;
+    PrintWriter out;
 
-    public ChatThread(Socket socket, Map<String, PrintWriter> chatClient) {
+    public ChatThread(Socket socket, Map<String, PrintWriter> chatClients) {
         this.socket = socket;
-        this.chatClients = chatClient;
+        this.chatClients = chatClients;
 
         //클라이언트가 생성될 때 클라이언트로 부터 아이디를 얻어오게 하고 싶어요.
         //각각 클라이언트와 통신 할 수 있는 통로얻어옴.
-        try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-        ) {
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //Client가 접속하자마 id를 보낸다는 약속!!
             id = in.readLine();
             //이때..  모든 사용자에게 id님이 입장했다라는 정보를 알려줌.
             broadcast(id + "님이 입장하셨습니다.");
             System.out.println("새로운 사용자의 아이디는 " + id + "입니다.");
 
             //동시에 일어날 수도..
-            synchronized (chatClient) {
+            synchronized (chatClients) {
                 chatClients.put(this.id, out);
             }
 
 
         } catch (Exception e) {
             System.out.println(e);
+            e.printStackTrace();
         }
     }
 
     @Override
     public void run() {
+        System.out.println(id + "사용자 채팅시작!!");
         //run
         //연결된 클라이언트가 메시지를 전송하면, 그 메시지를 받아서 다른 사용자들에게 보내줌..
         String msg = null;
@@ -80,6 +83,7 @@ class ChatThread extends Thread {
 
         } catch (IOException e) {
             System.out.println(e);
+            e.printStackTrace();
         } finally {
             synchronized (chatClients) {
                 chatClients.remove(id);
@@ -108,6 +112,7 @@ class ChatThread extends Thread {
     public void broadcast(String msg) {
         for (PrintWriter out : chatClients.values()) {
             out.println(msg);
+//            out.flush();
         }
     }
 }
