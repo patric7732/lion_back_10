ef50bb19cb0bbd6811240fd237600d950db5f2b0
carami <carami@nate.com>
2024-05-08 11:50:13 +0900

    add jdbc exam

---
+++ HappySQL/src/main/java/com/exam/jdbc/DeptDAO.java
@@ -0,0 +1,21 @@
+package com.exam.jdbc;
+
+public class DeptDAO {
+    //입력
+    public boolean addDept(int deptno, String dname,String loc ){
+        //매개변수로 받아올 값이 너무 많아요..  그럴때..  어떻게???   -- 가방처럼 값을 담을 객체를 만들어서 사용함.
+
+
+
+        return false;
+    }
+    //수정
+
+    //삭제
+
+    //모두 조회
+
+    //한 건 조회
+
+    //조건에 맞는 조회...
+}
---
+++ HappySQL/src/main/java/com/exam/jdbc/InsertTest.java
@@ -0,0 +1,54 @@
+package com.exam.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertTest {
+    public static void main(String[] args) {
+        //1.필요한 객체를 선언한다.
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            //3. 접속
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String dbUrl = "jdbc:mysql://localhost/exampledb";
+            String user = "carami";
+            String password = "kang1234";
+            conn = DriverManager.getConnection(dbUrl,user,password);
+            //4. 쿼리작성
+            String sql = "insert into dept(deptno,dname,loc) values (60,?,?)";
+
+            ps = conn.prepareStatement(sql);
+
+            ps.setString(1,"개발부");
+            ps.setString(2,"인천");
+
+            //5. 실행.
+            int count = ps.executeUpdate();
+            System.out.println(count+"건 입력했습니다!!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            //2. 닫아준다.
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+
+    }
+}
