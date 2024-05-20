b1b2cd59f9b6fc222bd75edbdb4ce50e963bd960
carami <carami@nate.com>
2024-05-20 15:59:53 +0900

    update exam

--- springmvc/src/main/java/org/example/springmvc/controller/MyController.java
+++ springmvc/src/main/java/org/example/springmvc/controller/MyController.java
@@ -1,10 +1,10 @@
 package org.example.springmvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
@@ -21,5 +21,53 @@ public class MyController {
     @GetMapping("/con")
     public String contact(){
         return "contact";
+    }
+
+    @ResponseBody
+    @GetMapping("rest")
+    public String rest(){
+        return "restbody test!!!";
+    }
+
+//    @GetMapping("greeting")
+//    public String greet(String name, int age, HttpServletRequest request){
+//        //http://localhost:8888/greeting?name=carami
+//        //?name=carami&age=20 쿼리문자열
+//        System.out.println(name);
+//        System.out.println(age);
+//
+//        System.out.println("request:::"+request.getParameter("name"));
+//        System.out.println("request:::"+request.getParameter("age"));\
+//        return "greeting";
+//    }
+
+//    @GetMapping("greeting")
+//    public String greet(
+//            @RequestParam String name,
+//            @RequestParam(name="age", required = false, defaultValue = "10")int age,Model model){
+//        //http://localhost:8888/greeting?abc=carami
+//        //?name=carami&age=20 쿼리문자열
+//        System.out.println(name);
+//        System.out.println(age);
+//
+//        model.addAttribute("name",name);
+//
+//        //이때 얻은 값을..  서비스
+//
+//        return "greeting";
+//    }
+
+    @PostMapping("greeting")
+    public ModelAndView greet(@RequestParam String name, ModelAndView modelAndView){
+        //http://localhost:8888/greeting?abc=carami
+        //?name=carami&age=20 쿼리문자열
+        System.out.println(name);
+
+        modelAndView.addObject("name",name);
+        modelAndView.setViewName("greeting");
+
+//        request.setAttribute("name",name);
+
+        return modelAndView;
     }
 }
